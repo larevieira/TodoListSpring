d1e8450d73c9cd9a0d79d26c763e114d0775435d
Vieira <larissa.vieira@capgemini.com>
2021-09-24 16:40:42 -0300

    correcoes na api

--- src/main/java/com/vieira/todolist/controller/TodoListController.java
+++ src/main/java/com/vieira/todolist/controller/TodoListController.java
@@ -8,7 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,13 +23,18 @@ import com.vieira.todolist.daos.TodoListDAO;
 import com.vieira.todolist.model.TodoList;
 import com.vieira.todolist.service.TodoListService;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
+@RequestMapping("todoList")
 public class TodoListController {
 
 	@Autowired
 	private TodoListService todoListService;
+	
+	@Autowired
+	TodoListDAO listDAO;
 
-	@RequestMapping(method = RequestMethod.POST, value = "/todoList", consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping()
 	public ResponseEntity<TodoList> save(@RequestBody TodoList todo) {
 		System.out.println("cadastrou tarefa " + todo.getDescription());
 
@@ -32,11 +42,9 @@ public class TodoListController {
 
 		return new ResponseEntity<TodoList>(teste, HttpStatus.CREATED);
 
-		// todoListDAO.save(todo);
-		// return "products/ok";
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "/busca", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping()
 	public ResponseEntity<Collection<TodoList>> buscar() {
 
 		Collection<TodoList> listTodo = todoListService.findAll();
@@ -44,26 +52,26 @@ public class TodoListController {
 		return new ResponseEntity<>(listTodo, HttpStatus.OK);
 	}
 
-	@RequestMapping(method = RequestMethod.DELETE, value = "/delete/{id}")
+	
+	@DeleteMapping("/{id}")
 	public ResponseEntity<TodoList> deleteList(@PathVariable Integer id) {
 		System.out.println("deletou tarefa ");
-
-	
-		TodoList testeExcluir = (TodoList) todoListService.findById(id);
 		
-		 if(testeExcluir == null) { 
-			 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		  }
-		
-
-		todoListService.delete(testeExcluir);
+		listDAO.deleteById(id);
 
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
-	@RequestMapping(method = RequestMethod.PUT, value = "/edit", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<TodoList> edit(@RequestBody TodoList todo) {
+	@PutMapping("/{id}")
+	public ResponseEntity<TodoList> edit(@PathVariable Integer id, @RequestBody TodoList todo) {
 
+		if(todo.getId() != id) {
+			return ResponseEntity.badRequest().build();
+		}
+		if(!listDAO.existsById(id)){
+			return ResponseEntity.notFound().build();
+		}
+		todo.setId(id);
 		TodoList todoEdit = todoListService.edit(todo);
 		System.out.println("alterado");
 		return new ResponseEntity<>(todoEdit, HttpStatus.OK);
